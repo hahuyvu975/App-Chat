a0b595db5425ab206f7da22c2ba0a1261a76e38b
hahuyvu975 <hahuyvu975@gmail.com>
2022-11-16 16:57:03 +0700

    first commit

---
+++ zalo-fe/src/components/Cat.js
@@ -0,0 +1,10 @@
+import {Avatar} from '@mui/material';
+import React from 'react'
+
+const Cat = () => {
+  return (
+    <Avatar src='https://th.bing.com/th/id/OIP.uqZMCYNKrsubom3k-zC3xwHaFj?pid=ImgDet&rs=1'/>
+  )
+}
+
+export default Cat
---
+++ zalo-fe/src/pages/dashboard/Chats.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Box, IconButton, Stack, Typography } from '@mui/material'
+import { CircleDashed } from 'phosphor-react';
+
+const Chats = () => {
+    return (
+        <Box
+            sx={{
+                position: "relative",
+                height: "100vh",
+                width: 320, 
+                backgroundColor: "#F8FAFF",
+                boxShadow: "0px 0px 2px rgba(0, 0, 0, 0.25)",
+            }}
+        >
+            <Stack p = {3}>
+                <Stack
+                    direction="row"
+                    alignItems={"center"}
+                    justifyContent="space-between"
+                >
+                    <Typography variant='h5'>
+                        Chats
+                    </Typography>
+                    <IconButton>
+                        <CircleDashed />
+                    </IconButton>
+                </Stack>
+            </Stack>
+
+        </Box>
+    );
+};
+
+export default Chats;
